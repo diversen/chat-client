2ce0d5e44b17660bd31b36eed4dfc36d6a02b24c
Dennis Iversen <dennis.iversen@gmail.com>
2025-06-08 00:13:42 +0200

    markdown should open links in new window

--- chat_client/static/js/markdown.js
+++ chat_client/static/js/markdown.js
@@ -6,7 +6,39 @@ import { markdownItTable } from '/static/dist/markdown-it-table.js';
 const md = markdownit('commonmark');
 const mdNoHTML = markdownit('commonmark', { html: false });
 
+
+
 md.use(markdownItTable);
 mdNoHTML.use(markdownItTable);
+
+// keep a reference to the default renderer
+const defaultLinkOpen =
+    md.renderer.rules.link_open ||
+    function (tokens, idx, options, env, self) {
+        return self.renderToken(tokens, idx, options);
+    };
+
+
+const mdLinkOpen = function (tokens, idx, options, env, self) {
+    const token = tokens[idx];
+
+    // target="_blank"
+    if (token.attrIndex('target') < 0) {
+        token.attrPush(['target', '_blank']);
+    } else {
+        token.attrs[token.attrIndex('target')][1] = '_blank';
+    }
+
+    // rel="noopener noreferrer" (recommended for security)
+    if (token.attrIndex('rel') < 0) {
+        token.attrPush(['rel', 'noopener noreferrer']);
+    }
+
+    return defaultLinkOpen(tokens, idx, options, env, self);
+};
+
+md.renderer.rules.link_open = mdLinkOpen;
+mdNoHTML.renderer.rules.link_open = mdLinkOpen;
+
 
 export { md, mdNoHTML };
