8305970646dc2a37f640b0d8fa19f79678fc5999
Dennis Iversen <dennis.iversen@gmail.com>
2026-02-13 21:49:43 +0100

    importmap includes js files in sub dirs

--- bin/importmap_generate.py
+++ bin/importmap_generate.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
-import os
 import json
+from pathlib import Path
 
 
 def generate_import_map(base_dirs):
     import_map = {"imports": {}}
     for base_dir in base_dirs:
-        # Strip "chat_client" or any top-level prefix
-        relative_dir = base_dir.split("chat_client/")[-1]  # removes "chat_client/"
-        import_dir = f"/{relative_dir}"  # ensures leading slash
+        base_path = Path(base_dir)
+        # Strip "chat_client" or any top-level prefix.
+        relative_dir = str(base_path).split("chat_client/")[-1]
+        import_dir = f"/{relative_dir}"
 
-        js_files = [f for f in os.listdir(base_dir) if f.endswith(".js")]
+        js_files = sorted(base_path.rglob("*.js"))
         for js_file in js_files:
-            key = f"{import_dir}/{js_file}"
+            relative_js_path = js_file.relative_to(base_path).as_posix()
+            key = f"{import_dir}/{relative_js_path}"
             value = key + "?version={{ version }}"
             import_map["imports"][key] = value
     return json.dumps(import_map, indent=4)
