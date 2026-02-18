6616cc90cc5d3fe2b56de3e071b63da1d92bf656
Dennis Iversen <dennis.iversen@gmail.com>
2026-02-18 20:07:05 +0100

    fix flake8. use only 1 process.

--- bin/check_code.sh
+++ bin/check_code.sh
@@ -3,7 +3,7 @@ echo "Running black ..."
 black . --config pyproject.toml --extend-exclude '/chat_client/migrations/'
 
 echo "Running mypy ..."
-mypy  --config-file pyproject.toml chat_client
+mypy  --config-file pyproject.toml chat_client 
 
 echo "Running flake8 ..."
-flake8 . --config .flake8
+flake8 . -j 1 --config .flake8
